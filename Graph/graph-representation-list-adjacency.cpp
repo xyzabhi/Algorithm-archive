ee40e12a8adfd83583b3abd54ae38d6e8752f95c
xyzabhi <abhinavkumar8083@gmail.com>
2020-03-21 05:54:33 +0530

    Graph representation using list adjacency

--- Graph/graph-representation-list-adjacency.cpp
+++ Graph/graph-representation-list-adjacency.cpp
@@ -1,7 +1,36 @@
 #include<bits/stdc++.h>
+using namespace std;
+//addEdge function to add edge between vertices
+
+void addEdge(vector<int>adj[],int u,int v)
+{
+    adj[v].push_back(u);//undirected graph (v----u)
+    adj[u].push_back(v);//undirected graph (u----v)
+}
+
+//function for printing the graph in the form of list of vectices a vertex connected
+
+void printGraph(vector<int>adj[],int V)
+{
+    for(int v=0;v<V;v++)
+    {
+        cout<<"Adjacency list of vertex "<<v<<"\n head";
+        for(auto x:adj[v])
+        cout<<"->"<<x;
+        cout<<endl;
+    }
+}
 int main()
 {
-    using namespace std;
-    cout<<"Hello"<<endl;
-    return 0;
+    int V=5;
+    vector<int> adj[V];
+    addEdge(adj,0,1);
+    addEdge(adj,0,4);
+    addEdge(adj,1,2);
+    addEdge(adj,1,3);
+    addEdge(adj,1,4);
+    addEdge(adj,2,3);
+    addEdge(adj,3,4);
+    addEdge(adj,3,1);
+    printGraph(adj,V);
 }
